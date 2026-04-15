a6070a1b3a5a079cb927c3fd3d6472d556cf7cd9
Higor Machado <higor.machado2011@gmail.com>
2026-04-14 22:43:45 -0300

    feat/configuração do orm pronta para ser aplicada na nossa aplicação e ser consumida dentro dos repository

--- src/db/database/database.module.ts
+++ src/db/database/database.module.ts
@@ -1,6 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { DRIZZLE, DATABASE_URL } from './database.constants';
 import { drizzle } from 'drizzle-orm/node-mssql';
+import { connect } from 'mssql';
+import type { config as MsSqlConfig } from 'mssql';
 import * as schema from '../schemas/index';
 
 @Global()
@@ -9,8 +11,22 @@ import * as schema from '../schemas/index';
     {
       provide: DRIZZLE,
       inject: [],
-      useFactory: () => {
-        return drizzle(DATABASE_URL, { schema: schema });
+      useFactory: async () => {
+        const dbConfig: MsSqlConfig = {
+          server: 'srv-bd-1',
+          port: 1433,
+          user: 'alunos_des225',
+          password: '123',
+          database: 'des225_higor',
+          options: {
+            encrypt: false,
+            trustServerCertificate: true,
+          },
+        };
+
+        const pool = await connect(dbConfig);
+
+        return drizzle({ client: pool, schema: schema });
       },
     },
   ],
