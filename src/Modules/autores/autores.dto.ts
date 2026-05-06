66634281c88b6871dffec535021ddf4dea842cce
Higor Machado <higor.machado2011@gmail.com>
2026-05-05 22:38:55 -0300

    feat/aplicado validação com as bibliotecas class-validator e class-transformer no DTO da criação do autor

--- src/Modules/autores/autores.dto.ts
+++ src/Modules/autores/autores.dto.ts
@@ -1,5 +1,23 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
 export class CriarAutorDto {
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
+  @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
+  @Transform(({ value }) => value.trim())
   nome: string;
+
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
+  @IsNotEmpty({ message: 'O email é obrigatório' })
+  @Transform(({ value }) => value.trim())
   email: string;
 }
 
