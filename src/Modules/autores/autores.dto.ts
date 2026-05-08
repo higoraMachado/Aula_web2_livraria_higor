d2de81e17ef1782ef978ed5564c98167cb5fad0d
Higor Machado <higor.machado2011@gmail.com>
2026-05-07 22:49:09 -0300

    07/05

--- src/Modules/autores/autores.dto.ts
+++ src/Modules/autores/autores.dto.ts
@@ -4,6 +4,7 @@ import {
   IsEmail,
   MinLength,
   MaxLength,
+  IsOptional,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
 
@@ -12,16 +13,45 @@ export class CriarAutorDto {
   @IsNotEmpty({ message: 'O nome é obrigatório' })
   @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
   @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => { 
+    const valor = typeof value;
+    if (valor === 'string') {
+      return value.trim();
+    }
+  })
   nome: string;
 
   @IsEmail({}, { message: 'O email deve ser um email válido' })
   @IsNotEmpty({ message: 'O email é obrigatório' })
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => { 
+    const valor = typeof value;
+    if (valor === 'string') {
+      return value.trim();
+    }
+  })
   email: string;
 }
 
 export class AtualizarAutorDto {
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsOptional()
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
+  @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
+  @Transform(({ value }) => { 
+    const valor = typeof value;
+    if (valor === 'string') {
+      return value.trim();
+    }
+  })
   nome: string;
+
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
+  @IsOptional()
+  @Transform(({ value }) => { 
+    const valor = typeof value;
+    if (valor === 'string') {
+      return value.trim();
+    }
+  })
   email: string;
 }
